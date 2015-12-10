961d5fcfa38324588e74f28485d0f9cabdeb107f
Shane <shane8641@live.com>
2015-12-10 00:01:42 -0500

    added class Game

---
+++ GameStore/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore
+{
+    class Game
+    {
+        private string name;
+        private string genre;
+        private double price;
+        private string recAge;
+    }
+}
