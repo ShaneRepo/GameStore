915b130fd9f299d75ba316446c46c363aacb3b6f
Shane <shane8641@live.com>
2015-12-10 17:26:11 -0500

    added Computer class

---
+++ GameStore/Computer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore
+{
+    class Computer : Game
+    {
+        // Field
+        private string _platform;
+        // default constructor
+        public Computer()
+        {
+
+        }
+        // constuctor with params that calls base constuctor
+        public Computer(string plat,string name,string genre, double price, string recAge, string players)
+            : base("name", "genre", price, "recAge", "players")
+        {
+            _platform = plat;
+        }
+        // Platform property
+        public string Platform
+        {
+            get { return _platform; }
+            set { _platform = value; }
+        }
+    }
+}
--- GameStore/Game.cs
+++ GameStore/Game.cs
@@ -13,15 +13,20 @@ namespace GameStore
         private string _genre;
         private double _price;
         private string _recAge;
-        private string _players; // a string b/c 2+ 4+ ect..
-        // constructor for the Game class, must include all fields
-        public Game(string n, string g, double p, string a,string pl)
+        private string _players; 
+        // default constructor
+        public Game()
         {
-            this._name = n;
-            this._genre = g;
-            this._price = p;
-            this._recAge = a;
-            this._players = pl;
+
+        }
+        // constructor for the Game class, must include all fields
+        public Game(string name, string genre, double price, string recAge,string players)
+        {
+            _name = name;
+            _genre = genre;
+            _price = price;
+            _recAge = recAge;
+            _players = players;
         }
         // Name property
         public string Name
