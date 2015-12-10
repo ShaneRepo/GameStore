cd595edaf81de33083fd462eeafb69762ffb458f
Shane <shane8641@live.com>
2015-12-10 17:57:11 -0500

    added lists

--- GameStore/Form1.Designer.cs
+++ GameStore/Form1.Designer.cs
@@ -28,12 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.listBoxGames = new System.Windows.Forms.ListBox();
+            this.labelListboxGames = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.AutoSize = true;
+            this.labelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.Location = new System.Drawing.Point(278, 9);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(266, 24);
+            this.labelTitle.TabIndex = 0;
+            this.labelTitle.Text = "Welcome to the game store";
+            // 
+            // listBoxGames
+            // 
+            this.listBoxGames.FormattingEnabled = true;
+            this.listBoxGames.Location = new System.Drawing.Point(436, 203);
+            this.listBoxGames.Name = "listBoxGames";
+            this.listBoxGames.Size = new System.Drawing.Size(352, 264);
+            this.listBoxGames.TabIndex = 1;
+            // 
+            // labelListboxGames
+            // 
+            this.labelListboxGames.AutoSize = true;
+            this.labelListboxGames.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelListboxGames.Location = new System.Drawing.Point(544, 180);
+            this.labelListboxGames.Name = "labelListboxGames";
+            this.labelListboxGames.Size = new System.Drawing.Size(137, 20);
+            this.labelListboxGames.TabIndex = 2;
+            this.labelListboxGames.Text = "Game Selection";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(53, 224);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 3;
+            this.buttonStart.Text = "Start App";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(53, 273);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 4;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(53, 335);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoad.TabIndex = 5;
+            this.buttonLoad.Text = "Load";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(931, 743);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.labelListboxGames);
+            this.Controls.Add(this.listBoxGames);
+            this.Controls.Add(this.labelTitle);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.ListBox listBoxGames;
+        private System.Windows.Forms.Label labelListboxGames;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonLoad;
     }
 }
 
--- GameStore/Form1.cs
+++ GameStore/Form1.cs
@@ -7,14 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace GameStore
 {
     public partial class Form1 : Form
     {
+        // global stream objects
+        StreamWriter outputFile;
+        StreamReader inputFile;
+        // global list object to hold computer game objects
+        List<Computer> gameList = new List<Computer>();
         public Form1()
         {
             InitializeComponent();
         }
+
     }
 }
